477d5687eaffad11e3f636c2e7a09ecb6763a5dc
Jose Soto <jsoto7087@gmail.com>
2022-12-17 01:17:23 -0500

    fetch image and displays

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [data, setData] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(()=> {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data.message)
+                setIsLoaded(true)
+            })
+    }, [])
+
+    if (!isLoaded) return <p>Loading...</p>
+    return (
+        <div>
+            <img alt="A Random Dog" src={data}/>
+        </div>
+    )
+}
+
+export default App;
